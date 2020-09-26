c8d33bb178b61bcaaf00d79df6ed348d1084a2e5
fabian <fabian.moncayo94@gmail.com>
2020-09-25 23:58:36 -0500

    tablas

---
+++ Tablas.py
@@ -0,0 +1,24 @@
+#Script Tabla
+
+"""
+programa que solicite al usuario el número de una tabla de
+multiplicar
+"""
+
+#INPUTS
+
+print("determine la tabla de un numero")
+print("digite un numero")
+n=int(input())
+
+#PROCESS
+print(n, " x 1 =", n*1)
+print(n, " x 1 =", n*2)
+print(n, " x 1 =", n*3)
+print(n, " x 1 =", n*4)
+print(n, " x 1 =", n*5)
+print(n, " x 1 =", n*6)
+print(n, " x 1 =", n*7)
+print(n, " x 1 =", n*8)
+print(n, " x 1 =", n*9)
+print(n, " x 1 =", n*10)
