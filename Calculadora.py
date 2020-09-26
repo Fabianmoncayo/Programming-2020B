70f139c9c7273b83308940197c6e3df567478340
fabian <fabian.moncayo94@gmail.com>
2020-09-25 21:33:25 -0500

    calculadora

---
+++ Calculadora.py
@@ -0,0 +1,17 @@
+#Script: calculadora
+
+""" Operaciones basicas con 2 numeros
+"""
+n1=5
+n2=6
+suma=n1+n2
+resta=n1-n2
+multiplicacion=n1*n2
+division=n1/n2
+print ("el resultado de la suma es ", suma)
+print ("el resultado de la resta es ", resta)
+print ("el resultado de la multiplicacion es ", multiplicacion)
+print ("el resultado de la division es ", division)
+
+
+
