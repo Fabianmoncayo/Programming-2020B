c66e87348128cfad9e3751b5211a122c61596fbd
fabian <fabian.moncayo94@gmail.com>
2020-09-25 22:33:31 -0500

    temperatura

---
+++ Temperatura.py
@@ -0,0 +1,18 @@
+#script: temperatura
+
+''' programa para percibir temperatura
+'''
+#INPUT
+
+n1=0
+n2=0
+
+print ('digite la temperatura en grados centrigrados')
+
+#PROCESS
+
+n1= int (input())
+
+n2= n1 + 273.15
+
+print ('la temperatura en grados Kelvin es ',n2)
